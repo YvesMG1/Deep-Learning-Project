2d1c2d75bacf0f8ca99406f2b13cc9a93fb66954
fretherick <78920883+fretherick@users.noreply.github.com>
2024-01-12 15:11:52 +0100

    BINN Final
    binn final and the binn models

---
+++ util_for_examples.py
@@ -0,0 +1,33 @@
+### This is a file given by the BINN documentation and is used to make sure that the input data for the binn has the right format 
+from sklearn import preprocessing
+import pandas as pd
+import numpy as np
+
+
+def fit_data_matrix_to_network_input(data_matrix: pd.DataFrame, features, feature_column="Protein") -> pd.DataFrame:
+    dm = data_matrix.copy()
+    nr_features_in_matrix = len(dm.index)
+    if len(features) > nr_features_in_matrix:
+        print("hello")
+        features_df = pd.DataFrame(features, columns=[feature_column])
+        dm = dm.merge(
+            features_df, how='right', on=feature_column)
+    if len(features) > 0:
+        print("dasdf")
+        dm.set_index(feature_column, inplace=True)
+        dm = dm.loc[features]
+    return dm
+
+
+def generate_data(data_matrix: pd.DataFrame, design_matrix: pd.DataFrame):
+    GroupOneCols = design_matrix[design_matrix['group']
+                                 == 1]['sample'].values
+    GroupTwoCols = design_matrix[design_matrix['group']
+                                 == 2]['sample'].values
+
+    df1 = data_matrix[GroupOneCols].T
+    df2 = data_matrix[GroupTwoCols].T
+    y = np.array([0 for _ in GroupOneCols] + [1 for _ in GroupTwoCols])
+    X = pd.concat([df1, df2]).fillna(0).to_numpy()
+    X = preprocessing.StandardScaler().fit_transform(X)
+    return X, y
